115be993fd8c3815b3ac5341ec6f6f4395fa3807
Joe <jmpease21@gmail.com>
2017-10-30 09:27:39 -0400

    different saturday messages

--- src/saturday/saturday.go
+++ src/saturday/saturday.go
@@ -10,19 +10,19 @@ func main() {
 	today := time.Now().Weekday()
 	switch time.Saturday {
 	case today + 0:
-		fmt.Println("Today.")
+		fmt.Println("Today is Saturday!")
 	case today + 1:
-		fmt.Println("Tomorrow.")
+		fmt.Println("Saturday is Tomorrow!")
 	case today + 2:
-		fmt.Println("In two days.")
+		fmt.Println("It's Saturday In two days.")
   case today + 3:
-    fmt.Println("In three days.")
+    fmt.Println("It's Saturday In three days.")
   case today + 4:
-    fmt.Println("In four days.")
+    fmt.Println("It's Saturday In four days.")
   case today + 5:
-    fmt.Println("In five days.")
+    fmt.Println("It's Saturday In five days.")
   case today + 6:
-    fmt.Println("In six days.")
+    fmt.Println("It's Saturday In six days.")
 	default:
 		fmt.Println("Too far away.")
 	}
